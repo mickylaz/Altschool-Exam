d0bddef31595aa3c9b9f0e499a114631bae8d57d
Michael <95645964+mickylaz@users.noreply.github.com>
2022-07-05 08:45:00 +0000

    space bar

--- src/assignment.js
+++ src/assignment.js
@@ -15,7 +15,7 @@ const assignment = {};
  */
 function sumOfNumbers(arrayOfNumbers) {
     let sum = 0;
-    for(let i = 0; i < arrayOfNumbers.length; i++){
+    for (let i = 0; i < arrayOfNumbers.length; i++){
         sum += arrayOfNumbers[i];
     }
     return sum;
